9573a1ff3984a8bf9dc3fd2cffbca389fa27a976
Ivan Mijatović <mijatovicivan1@gmail.com>
2020-04-08 09:02:01 +0200

    c#,zbrajanje

---
+++ Zbrajanje 2 broja/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Zbrajanje_2_broja
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite prvi broj!");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Unesite drugi broj!");
+            int b = int.Parse(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine(c);
+
+
+
+        }
+    }
+}
