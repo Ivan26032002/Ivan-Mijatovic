9587fd51ff2c2736ef0bf403b1b950f4f0a5f66a
Ivan Mijatović <mijatovicivan1@gmail.com>
2020-04-16 17:02:21 +0200

    i++

---
+++ skola/Program.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+
+namespace skola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("upisi do koliko zelis ici");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for ( int i = 1 ;i <= n; i++) 
+            
+                {if ( (i % 4 == 3) || (i % 4 == 0))
+                
+
+
+                    Console.WriteLine(i + " ");
+                
+
+
+
+
+                }
+
+
+
+
+        }
+        
+}
+}
+
+...
+int i;
+            for (i = 1;i <= 100; i++) 
+            
+                {
+                if (i % 5 == 0)
+                    Console.WriteLine(i + " ");
+
+
+
+                }
+...
+    for (i = 1;i <= 10; i++)
+    console.writeline(i + " ");
